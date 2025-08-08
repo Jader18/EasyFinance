54e88a2e0bfaf806e085d3b0ea7fbb98979a23da
MathiusZamora <mathiuszamora2@gmail.com>
2025-08-08 15:49:42 -0600

    back button added to chartScreen

--- app/src/main/java/com/jader/easyfinance/ui/screens/ChartsScreen.kt
+++ app/src/main/java/com/jader/easyfinance/ui/screens/ChartsScreen.kt
@@ -11,7 +11,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -101,7 +105,16 @@ fun ChartsScreen(
         modifier = modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
-                title = { Text("Gráficos") }
+                title = { Text("Gráficos") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver"
+                        )
+                    }
+                }
+
             )
         }
     ) { innerPadding ->
